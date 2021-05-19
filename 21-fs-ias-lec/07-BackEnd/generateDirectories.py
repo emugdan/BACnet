66667126f3aeb6dcb98701e119af8fe8998b4538
Vera <vera.benz@stud.unibas.ch>
2021-05-19 13:19:46 +0200

    generateDirectories generates the directory of the feeds now

--- 21-fs-ias-lec/07-BackEnd/generateDirectories.py
+++ 21-fs-ias-lec/07-BackEnd/generateDirectories.py
@@ -1,33 +1,67 @@
 import sys
 
+# add the lib to the module folder
 sys.path.append("lib")
 
-from Feed import Feed
+import os
+import crypto
+import feed
+import time
 
-# TODO: villicht das in Feed-Ordner integriere
-def generateDirectories():
-    # set a name to a feed
-    Feeds = {}
 
-    Feeds['yasmin'] = Feed.Feed("yasmin")
-    Feeds['esther'] = Feed.Feed("esther")
-    Feeds['vera'] = Feed.Feed("vera")
-    Feeds['pascal'] = Feed.Feed("pascal")
-    Feeds['phillip'] = Feed.Feed("phillip")
-    Feeds['sebastian'] = Feed.Feed("sebastian")
+class DummyFeeds:
 
-    Feeds['aline'] = Feed.Feed("aline")
-    Feeds['ben'] = Feed.Feed("ben")
-    Feeds['caroline'] = Feed.Feed("caroline")
-    Feeds['david'] = Feed.Feed("david")
-    Feeds['eveline'] = Feed.Feed("eveline")
-    Feeds['fitzgerald'] = Feed.Feed("fitzgerald")
-    Feeds['georgia'] = Feed.Feed("georgia")
-    Feeds['henry'] = Feed.Feed("henry")
-    Feeds['isabelle'] = Feed.Feed("isabelle")
-    Feeds['julius'] = Feed.Feed("julius")
+    def generate(self, name):
+        if not os.path.isdir("data"):
+            os.mkdir("data")
 
-    # Feeds erstellen
-    for name, feed in Feeds.items():
-        feed.generateOwnFeed()
+        if not os.path.isdir("data/" + name):
+            os.mkdir("data/" + name)
 
+        # Generate a key pair
+
+        # generate a feed
+        digestmod = "sha256"
+        h, signer = None, None
+
+        # Generate a Key if non exists
+        if not os.path.isfile("data/" + name + "/" + name + "-secret.key"):
+            print("Create " + name + "'s key pair at data/" + name + "/" + name + "-secret.key")
+            h = crypto.HMAC(digestmod)
+            h.create()
+            with open("data/" + name + "/" + name + "-secret.key", "w") as f:
+                f.write('{\n  ' + (',\n '.join(h.as_string().split(','))[1:-1]) + '\n}')
+                signer = crypto.HMAC(digestmod, h.get_private_key())
+
+        print("Read " + name + "'s secret key.")
+        with open("data/" + name + "/" + name + "-secret.key", 'r') as f:
+            key = eval(f.read())
+            h = crypto.HMAC(digestmod, key["private"], key["feed_id"])
+            if sys.platform.startswith("linux"):
+                signer = crypto.HMAC(digestmod, bytes.fromhex(h.get_private_key()))
+            else:
+                signer = crypto.HMAC(digestmod, h.get_private_key())
+
+        print("Create or load " + name + "'s feed at data/" + name + "/" + name + "-feed.pcap")
+        myFeed = feed.FEED(fname="data/" + name + "/" + name + "-feed.pcap", fid=h.get_feed_id(),
+                           signer=signer, create_if_notexisting=True, digestmod=digestmod)
+
+    def generateDirectories(self):
+        self.generate("yasmin")
+        self.generate("esther")
+        self.generate("vera")
+        self.generate("pascal")
+        self.generate("phillip")
+        self.generate("sebastian")
+        self.generate("aline")
+        self.generate("ben")
+        self.generate("caronline")
+        self.generate("david")
+        self.generate("david")
+        self.generate("eveline")
+        self.generate("fitzgerald")
+        self.generate("georgia")
+        self.generate("henry")
+        self.generate("isabelle")
+        self.generate("julius")
+        self.generate("veri")
