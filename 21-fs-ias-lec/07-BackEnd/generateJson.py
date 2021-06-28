818d60a330f16e47499c0f2dd456df0da63802e5
emugdan <esther.mugdan@stud.unibas.ch>
2021-06-28 08:58:08 +0200

    removed todos

--- 21-fs-ias-lec/07-BackEnd/generateJson.py
+++ 21-fs-ias-lec/07-BackEnd/generateJson.py
@@ -35,11 +35,8 @@ def generateJson(personList, weAre):
         node['town'] = person.town
         node['language'] = person.language
         node['status'] = person.status
-        # TODO ESTHER
         node['activity level'] = person.get_activity()
-        # node['influencer_count'] = person.influencer_count
         node['influencer'] = person.influencer
-        # TODO ESTHER
         node['hopLayer'] = 10000
         nodes.append(node)
         for friend in followList:
