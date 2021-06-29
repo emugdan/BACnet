1808568bc1a33c72fc6a88dc4eb8ef7afe593a1b
pascalraphaelkunz <pasca.kunz@stud.unibas.ch>
2021-06-29 13:20:12 +0200

    update callToBackend.py as instructed

--- 21-fs-ias-lec/FrontEnd/socialgraph/utils/callToBackend.py
+++ 21-fs-ias-lec/FrontEnd/socialgraph/utils/callToBackend.py
@@ -60,7 +60,7 @@ def iterateThroughDirs(mainPersonName, mainPersonID):
                                 signer=signer, create_if_notexisting=True, digestmod=digestmod)
 
             # Feed objekt erstellen
-            feed_obj = Feed.Feed(key["feed_id"], my_feed)
+            feed_obj = Feed.Feed(key["feed_id"], my_feed, name)
 
             person = Person.Person(key["feed_id"], name, feed_obj)
             list_of_persons.append(person)
@@ -70,42 +70,17 @@ def iterateThroughDirs(mainPersonName, mainPersonID):
             if (sys.platform == "linux"):
                 if (name == mainPersonName and key["feed_id"] == mainPersonID):
                     mainPerson = person
+                    mainPerson.main = person
             else:
                 if (name == mainPersonName and key["feed_id"] == mainPersonID.encode("utf-8")):
                     mainPerson = person
+                    mainPerson.main = person
 
 
 
     return (mainPerson, list_of_persons)
 
 
-"""
-Create followList.
-"""
-def followList(list_of_persons):
-    for pers in list_of_persons:
-        follow_list = pers.feed.read_follow_from_feed()
-        birthday = pers.feed.readBirthdayFromFeed()
-        gender = pers.feed.readGenderFromFeed()
-        country = pers.feed.readCountryFromFeed()
-        town = pers.feed.readTownFromFeed()
-        language = pers.feed.readLanguageFromFeed()
-        status = pers.feed.readStatusFromFeed()
-
-        # Followliste vervollständigen
-        for follow_entry in follow_list:
-            for p in list_of_persons:
-                if follow_entry["Feed ID"] == p.id:
-                    pers.follow(follow_entry["Feed ID"], p.name)
-                    break
-
-        pers.print_follow_list()
-        pers.birthday = birthday
-        pers.gender = gender
-        pers.country = country
-        pers.town = town
-        pers.language = language
-        pers.status = status
 
 
 
@@ -116,12 +91,13 @@ def followCall(mainPersonName, mainPersonID,followPersonName, followPersonID):
     print(q)
     os.chdir(q.parent.parent.parent)
     (mainPerson, list_of_persons) = iterateThroughDirs(mainPersonName, mainPersonID)
+    helper(list_of_persons, mainPerson)
     if (sys.platform == "linux"):
         mainPerson.follow(followPersonID, followPersonName)
     else:
         mainPerson.follow(followPersonID.encode("utf-8"), followPersonName)
-    followList(list_of_persons)
-    generateJson(list_of_persons, mainPerson)
+
+
 
 """
 make update call to BackEnd. The function creates an updated JSON file.
@@ -130,9 +106,39 @@ def profileUpdateCall(mainPersonName, mainPersonID, data):
     print(q)
     os.chdir(q.parent.parent.parent)
     (mainPerson, list_of_persons) = iterateThroughDirs(mainPersonName, mainPersonID)
-    followList(list_of_persons)
+    helper(list_of_persons, mainPerson)
     mainPerson.put_attributes(data)
-    generateJson(list_of_persons, mainPerson)
+
+
+def helper(list_of_persons, main_person):
+    for pers in list_of_persons:  # for each person read the attributes from the entries in the feed
+        follow_list = pers.feed.read_follow_from_feed()
+        birthday = pers.feed.read_birthday_from_feed()
+        gender = pers.feed.read_gender_from_feed()
+        country = pers.feed.read_country_from_feed()
+        town = pers.feed.read_town_from_feed()
+        language = pers.feed.read_language_from_feed()
+        status = pers.feed.read_status_from_feed()
+        profile_pic = pers.feed.read_profile_pic_from_feed()
+        pers.list_of_persons = list_of_persons
+
+        for follow_entry in follow_list:  # creates follow list
+            for p in list_of_persons:  # go through all persons
+                if follow_entry["Feed ID"] == p.id:
+                    pers.follow(follow_entry["Feed ID"], p.name)
+                    break
+
+        pers.print_follow_list()  # for testing
+        pers.birthday = birthday
+        pers.gender = gender
+        pers.country = country
+        pers.town = town
+        pers.language = language
+        pers.status = status
+        pers.profile_pic = profile_pic
+
+        # tell each person who the mainPerson is and what persons we "know" (= have the feed at the moment)
+        pers.main = main_person
 
 
 
